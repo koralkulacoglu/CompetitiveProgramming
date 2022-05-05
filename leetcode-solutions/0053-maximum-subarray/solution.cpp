3b39c1a8f0a572617057da7d7c9bfc46c1d0d3a1
Koral Kulacoglu <kulacoglukoral@gmail.com>
2022-05-05 14:25:54 +0000

    Sync LeetCode submission Runtime - 132 ms (5.51%), Memory - 70.4 MB (99.97%)

---
+++ leetcode-solutions/0053-maximum-subarray/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1e9);
+        dp[0] = nums[0];
+        int ans = dp[0];
+        for (int i=1; i<n; i++) {
+            dp[i] = max(nums[i], dp[i-1] + nums[i]);
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
