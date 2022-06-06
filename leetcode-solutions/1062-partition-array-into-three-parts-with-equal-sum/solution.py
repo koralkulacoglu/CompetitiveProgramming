9894c4314a26d75f8c830bd4a39bbd3afd5a58e7
Koral Kulacoglu <kulacoglukoral@gmail.com>
2022-06-06 01:41:18 +0000

    Sync LeetCode submission Runtime - 326 ms (5.03%), Memory - 20.9 MB (100.00%)

---
+++ leetcode-solutions/1062-partition-array-into-three-parts-with-equal-sum/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canThreePartsEqualSum(self, arr: List[int]) -> bool:
+        val = sum(arr)
+        num = 0
+        val_3 = val//3
+        if val%3==0:
+            summ= 0
+            for i in range(len(arr)):
+                summ = summ+arr[i]
+                if summ == val_3:
+                    summ = 0
+                    num+=1
+                if num==2 and i<len(arr)-1:
+                    return True      
+        return False
+
