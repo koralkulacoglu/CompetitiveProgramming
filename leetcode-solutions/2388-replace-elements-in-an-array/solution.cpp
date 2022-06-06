36d5d5a14c83ed0a5615e6c52b420cb99b6e23fd
Koral Kulacoglu <kulacoglukoral@gmail.com>
2022-06-06 12:50:44 +0000

    Sync LeetCode submission Runtime - 815 ms (5.07%), Memory - 117.7 MB (98.55%)

--- leetcode-solutions/2388-replace-elements-in-an-array/solution.cpp
+++ leetcode-solutions/2388-replace-elements-in-an-array/solution.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& ops) {
-        int n = nums.size();
-        unordered_map<int, int> ptr;
-        for (int i=0; i<n; i++) {
-            ptr[nums[i]] = i;
+        int n=nums.size();
+        unordered_map<int, int> idx;
+        for (int i=0; i<n; i++) idx[nums[i]] = i;
+        for (vector<int> &i : ops) {
+            auto nodeHandler = idx.extract(i[0]);
+            nodeHandler.key() = i[1];
+            idx.insert(std::move(nodeHandler));
         }
-        for (vector<int> i : ops) {
-            int n1=i[0], n2=i[1];
-            auto nodeHandler = ptr.extract(n1);
-            nodeHandler.key() = n2;
-            ptr.insert(std::move(nodeHandler));
-        }
-        for (auto &i : ptr) {
-            int num = i.first;
-            int idx = i.second;
-            nums[idx] = num;
-        }
+        for (auto &i : idx) nums[i.second] = i.first;
         return nums;
     }
 };
