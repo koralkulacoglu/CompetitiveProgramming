6649534e8944ec8b8cf466b0047b7f7e4fdb45ff
Koral Kulacoglu <kulacoglukoral@gmail.com>
2022-06-08 02:01:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.1 MB (100.00%)

---
+++ leetcode-solutions/1013-fibonacci-number/solution.cpp
@@ -0,0 +1,15 @@
+vector<int> f(31, 0);
+
+class Solution {
+public:
+    int fib(int n) {
+        if (f[0] == 0) {
+            f[0] = 0;
+            f[1] = 1;
+            for (int i=2; i<31; i++) {
+                f[i] = f[i-1] + f[i-2];
+            }
+        }
+        return f[n];
+    }
+};
