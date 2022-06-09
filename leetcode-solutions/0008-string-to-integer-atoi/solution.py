1cc3b6b1b318774f635bae2e327906ec0a7c1436
Koral Kulacoglu <kulacoglukoral@gmail.com>
2022-06-09 17:04:00 +0000

    Sync LeetCode submission Runtime - 72 ms (5.15%), Memory - 13.9 MB (100.00%)

---
+++ leetcode-solutions/0008-string-to-integer-atoi/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        s=s.lstrip() 
+        negative=False
+        if not s:return 0
+        if s[0]=="-":
+            negative=True
+            s=s[1:]
+        elif s[0]=="+":
+            negative=False
+            s=s[1:]
+        ans=0
+        for i in s:
+            if i not in '0123456789':
+                break
+            else:
+                ans=ans*10+int(i)
+        if negative:
+            ans=-ans
+        return max(min(ans,2**31 -1),-2**31)
+
