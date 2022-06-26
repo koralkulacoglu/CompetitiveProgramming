077e244316c233308636231fbab95f2fcca0cbf4
Koral Kulacoglu <kulacoglukoral@gmail.com>
2022-06-26 22:33:02 +0000

    Sync LeetCode submission Runtime - 645 ms (5.01%), Memory - 16.4 MB (100.00%)

---
+++ leetcode-solutions/0221-maximal-square/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if not matrix:
+            return 0
+        m = len(matrix)
+        n = len(matrix[0])
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        max_area = 0
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if matrix[i - 1][j - 1] == '1':
+                    dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1
+                    max_area = max(max_area, dp[i][j])
+        return max_area * max_area
+
