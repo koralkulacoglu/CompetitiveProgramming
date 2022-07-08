a97a95c51d6b8d9ba185e3c3fdb7b7bf50b1a005
Koral Kulacoglu <kulacoglukoral@gmail.com>
2022-07-08 05:05:06 +0000

    Sync LeetCode submission Runtime - 22 ms (5.44%), Memory - 10.3 MB (100.00%)

---
+++ leetcode-solutions/1380-number-of-closed-islands/solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> dx = {-1, 1, 0, 0};
+    vector<int> dy = {0, 0, -1, 1};
+    vector<vector<int>> a;
+    vector<vector<bool>> vis;
+
+    int n, m, ans;
+    bool dfs(int x, int y) {
+        vis[x][y] = true;
+        bool res=true;
+        for (int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx>=0 && nx<n && ny>=0 && ny<m && !vis[nx][ny] && !a[nx][ny]) {
+                if (!dfs(nx, ny)) {
+                    res = false;
+                }
+            }
+        }
+        if (x==n-1 || x==0 || y==m-1 || y==0) {
+            return false;
+        }
+        return res;
+    }
+    
+    int closedIsland(vector<vector<int>>& grid) {
+        a = grid;
+        n=a.size(), m=a[0].size();
+        vis.clear();
+        vis.resize(n, vector<bool>(m, false));
+        ans=0;
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<m; j++) {
+                if (!a[i][j] && !vis[i][j] && dfs(i, j)) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
