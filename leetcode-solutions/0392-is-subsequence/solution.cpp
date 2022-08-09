187901e08facc343e76a8759a0a262fbc20f8a7f
Koral Kulacoglu <kulacoglukoral@gmail.com>
2022-08-09 03:12:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 6.4 MB (100.00%)

--- leetcode-solutions/0392-is-subsequence/solution.cpp
+++ leetcode-solutions/0392-is-subsequence/solution.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int n=t.size(), m=s.size(), idx=0;
-        for (int i=0; i<n; i++) {
-            if (idx == n) return true;
-            if (s[idx] == t[i]) {
-                idx++;
+        int n=s.size(), m=t.size(), cnt=0;
+        if (n==0 && m==0) return true;
+        for (int i=0; i<m; i++) {
+            if (s[cnt] == t[i]) {
+                cnt++;
             }
+            if (cnt == n) return true;
         }
-        return idx == m;
+        return false;
     }
 };
