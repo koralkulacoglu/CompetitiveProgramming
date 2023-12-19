505fb637752a23b78d3811522df68f45fe18c70c
Koral Kulacoglu <kulacoglukoral@gmail.com>
2023-12-19 23:44:38 +0000

    Sync LeetCode submission Runtime - 14 ms (6.50%), Memory - 18 MB (100.00%)

---
+++ leetcode-solutions/0135-candy/solution.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();vector<int>candies(n,1);for (int i=1;i<n;i++)ratings[i]>ratings[i-1]?candies[i]=candies[i-1]+1:0;for (int i=n-2;i>=0;i--)ratings[i]>ratings[i+1]?candies[i]=max(candies[i],candies[i+1]+1):0;return accumulate(candies.begin(),candies.end(),0);
+    }
+};
+
