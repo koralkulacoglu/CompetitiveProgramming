5e5a6b7dc66edcf84b8414866590981d9c7f7fcf
Koral Kulacoglu <kulacoglukoral@gmail.com>
2024-01-09 14:39:30 +0000

    Sync LeetCode submission Runtime - 6 ms (0.64%), Memory - 8.6 MB (100.00%)

--- leetcode-solutions/0213-house-robber-ii/solution.cpp
+++ leetcode-solutions/0213-house-robber-ii/solution.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() == 1) return nums[0];
-        int n=nums.size();
-        vector<int> dp(n, 0);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-        for (int i=2; i<n-1; i++) {
-            dp[i] = max(dp[i-1], dp[i-2]+nums[i]);
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
+
+        // do dp starting at each i
+        int ans = 0;
+        for (int i=0; i<n; i++) {
+            vector<int> dp(n-1, 0);
+            dp[0] = nums[i];
+            dp[1] = max(nums[i], nums[(i+1)%n]);
+            for (int j=2; j<n-1; j++) {
+                dp[j] = max(dp[j-1], dp[j-2] + nums[(i+j)%n]);
+            }
+            ans = max(ans, dp[n-2]);
         }
-        
-        vector<int> dp2(n, 0);
-        dp2[0] = 0;
-        dp2[1] = nums[1];
-        for (int i=2; i<n; i++) {
-            dp2[i] = max(dp2[i-1], dp2[i-2]+nums[i]);
-        }
-        
-        return max(dp[n-2], dp2[n-1]);
+        return ans;
     }
 };
