d3fc3688b7fd1483b19587c3fe3aae82e1dab114
Koral Kulacoglu <kulacoglukoral@gmail.com>
2024-02-13 21:48:12 +0000

    Sync LeetCode submission Runtime - 48 ms (5.63%), Memory - 23.4 MB (84.92%)

--- leetcode-solutions/2231-find-first-palindromic-string-in-the-array/solution.cpp
+++ leetcode-solutions/2231-find-first-palindromic-string-in-the-array/solution.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     string firstPalindrome(vector<string>& words) {
-        for (string i : words) {
+        for (string &i : words) {
+            int n = i.size();
             bool wk = true;
-            for (int j=0; j<i.size()/2; j++) {
-                if (i[j] != i[i.size()-j-1]) {
+            for (int j=0; j<n/2; j++) {
+                if (i[j] != i[n-j-1]) {
                     wk = false;
                     break;
                 }
