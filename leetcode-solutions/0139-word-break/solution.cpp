4441ef18baed8f2bca0a59c196770d237d5184df
Koral Kulacoglu <kulacoglukoral@gmail.com>
2024-03-06 04:31:02 +0000

    Sync LeetCode submission Runtime - 23 ms (12.84%), Memory - 15.7 MB (50.22%)

--- leetcode-solutions/0139-word-break/solution.cpp
+++ leetcode-solutions/0139-word-break/solution.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    unordered_map<string, bool> words;
-    unordered_map<string, bool> seen;
-    bool recur(string s) {
-        int n=s.size();
-        if (n == 0) return true;
-        if (seen[s]) return false;
-        seen[s] = true;
-        string cur;
-        for (int i=0; i<n; i++) {
-            cur += s[i];
-            if (words[cur] && recur(s.substr(i+1))) return true;
-        }
-        return false;
-    }
-    
     bool wordBreak(string s, vector<string>& wordDict) {
-        for (string &i : wordDict) words[i] = true;
-        return recur(s);
+        map<string, bool> seen;
+        for (string i : wordDict) seen[i] = true;
+        int n = s.size();
+        vector<bool> dp(n, 0);
+        for (int i=0; i<n; i++) {
+            if (i != 0 && dp[i-1] != 1) continue;
+            string cur;
+            for (int j=i; j<n; j++) {
+                cur += s[j];
+                if (seen[cur]) dp[j] = true;
+            }
+        }
+        return dp[n-1];
     }
 };
