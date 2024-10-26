e48fb8b97a586842c4c16d978f378b11ca32b210
Koral Kulacoglu <kulacoglukoral@gmail.com>
2024-10-26 20:29:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.7 MB (83.79%)

---
+++ leetcode-solutions/0072-edit-distance/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        for (int i=0; i<=n; i++) dp[i][0] = i;
+        for (int j=0; j<=m; j++) dp[0][j] = j;
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=m; j++) {
+                char a = word1[i-1];
+                char b = word2[j-1];
+                if (a == b) dp[i][j] = dp[i-1][j-1];
+                else dp[i][j] = 1 + min(dp[i-1][j-1], min(dp[i-1][j], dp[i][j-1]));
+            }
+        }
+        return dp[n][m];
+    }
+};
+
