9f11b8576e04b8f48577a704b8fdd11fe9599fc2
Koral Kulacoglu <kulacoglukoral@gmail.com>
2024-11-03 04:19:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.8 MB (89.80%)

---
+++ leetcode-solutions/0343-integer-break/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if (n == 2) return 1;
+        if (n == 3) return 2;
+        
+        vector<int> dp(n+1, 0);
+        dp[1] = 1;
+        for (int i=2; i<=n; i++) {
+            dp[i] = i;
+            for (int j=1; j<i; j++) {
+                dp[i] = max(dp[i], dp[j]*dp[i-j]);
+            }
+        }
+
+        return dp[n];
+    }
+};
