ee6a1d668ab0ad8a8c3115ce24312be1a245da3e
Koral Kulacoglu <kulacoglukoral@gmail.com>
2025-08-19 01:00:25 +0000

    Sync LeetCode submission Runtime - 7 ms (11.35%), Memory - 111.1 MB (94.46%)

--- leetcode-solutions/2432-number-of-zero-filled-subarrays/solution.cpp
+++ leetcode-solutions/2432-number-of-zero-filled-subarrays/solution.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    long long zeroFilledSubarray(vector<int>& a) {
-        long long n=a.size(), zer=0, ans=0;
+    long long zeroFilledSubarray(vector<int>& nums) {
+        int n = nums.size();
+        long long ans = 0;
+        int zeros = 0;
         for (int i=0; i<n; i++) {
-            if (a[i] == 0) {
-                zer++;
-                ans += zer;
+            if (nums[i] == 0) {
+                zeros++;
+                ans += zeros;
             }
             else {
-                zer = 0;
+                zeros = 0;
             }
         }
+
         return ans;
     }
 };
