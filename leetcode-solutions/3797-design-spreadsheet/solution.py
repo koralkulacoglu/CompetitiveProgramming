a7a25595fb87f302b4e0a1c30335e6b0f7c68a55
Koral Kulacoglu <kulacoglukoral@gmail.com>
2025-08-27 18:31:55 +0000

    Sync LeetCode submission Runtime - 70 ms (87.75%), Memory - 23.7 MB (46.70%)

---
+++ leetcode-solutions/3797-design-spreadsheet/solution.py
@@ -0,0 +1,20 @@
+class Spreadsheet:
+    def __init__(self, rows: int):
+        self.mp = {}
+
+    def setCell(self, cell: str, value: int) -> None:
+        self.mp[cell] = value
+        
+    def resetCell(self, cell: str) -> None:
+        self.mp[cell] = 0
+
+    def getValue(self, formula: str) -> int:
+        return sum([(int(x) if x.isdigit() else (self.mp[x] if x in self.mp else 0)) for x in formula[1:].split('+')])
+        
+
+
+# Your Spreadsheet object will be instantiated and called as such:
+# obj = Spreadsheet(rows)
+# obj.setCell(cell,value)
+# obj.resetCell(cell)
+# param_3 = obj.getValue(formula)
