2cb870df6aa43fb4b4c2df4a2a384b8fe7d6dd44
Koral Kulacoglu <kulacoglukoral@gmail.com>
2025-09-18 21:30:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14 MB (79.79%)

---
+++ leetcode-solutions/1510-find-lucky-integer-in-an-array/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        map<int, int> freq;
+        for (int i : arr) freq[i]++;
+
+        int ans = -1;
+        for (auto i : freq) {
+            if (i.first == i.second) ans = i.first;
+        }
+
+        return ans;
+    }
+};
