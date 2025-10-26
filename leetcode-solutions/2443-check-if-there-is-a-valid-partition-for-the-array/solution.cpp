cae851b31084f487121d60a0b8deb080b734284f
Koral Kulacoglu <kulacoglukoral@gmail.com>
2025-10-26 01:58:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 88 MB (93.14%)

--- leetcode-solutions/2443-check-if-there-is-a-valid-partition-for-the-array/solution.cpp
+++ leetcode-solutions/2443-check-if-there-is-a-valid-partition-for-the-array/solution.cpp
@@ -1,15 +1,25 @@
+/*
+
+idea: keep a frequency map and when you insert i, check if invalid, if so move left pointer until dp[left] = true. if that works dp[i] = true, else dp[i] = false
+
+*/
+
+
 class Solution {
 public:
-    bool validPartition(vector<int>& a) {
-        int n = a.size();
+    bool validPartition(vector<int>& nums) {
+        int n = nums.size();
+
         vector<bool> dp(n+1, false);
         dp[0] = true;
-        for (int i=0; i<n; i++) {
+
+        for (int i=0; i<=n; i++) {
             if (!dp[i]) continue;
-            if (i < n-1 && a[i] == a[i+1]) dp[i+2] = true;
-            if (i < n-2 && a[i] == a[i+1] && a[i+1] == a[i+2]) dp[i+3] = true;
-            if (i < n-2 && a[i] == a[i+1]-1 && a[i+1] == a[i+2]-1) dp[i+3] = true;
+            if (i < n-1 && nums[i] == nums[i+1]) dp[i+2] = true;
+            if (i < n-2 && nums[i] == nums[i+1] && nums[i] == nums[i+2]) dp[i+3] = true;
+            if (i < n-2 && nums[i] == nums[i+1]-1 && nums[i+1] == nums[i+2]-1) dp[i+3] = true;
         }
+
         return dp[n];
     }
 };
