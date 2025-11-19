60b17a371f4d9a199e93cccf2c63466c46adfd5e
Koral Kulacoglu <kulacoglukoral@gmail.com>
2025-11-19 01:12:31 +0000

    Sync LeetCode submission Runtime - 11 ms (5.31%), Memory - 15.9 MB (23.94%)

--- leetcode-solutions/2274-keep-multiplying-found-values-by-two/solution.cpp
+++ leetcode-solutions/2274-keep-multiplying-found-values-by-two/solution.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int original) {
-        unordered_map<int, int> freq;
-        for (int &i : nums) freq[i]++;
-        while (freq[original]) {
-            freq[original]--;
-            original *= 2;
-            if (!freq[original]) return original;
-            freq[original]++;
-        }
+        unordered_map<int, bool> seen;
+        for (int i : nums) seen[i] = true;
+        while (seen[original]) original *= 2;
         return original;
     }
 };
