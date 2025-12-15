82251a217b8b69d29cc860ef40cf5f4f279e6db1
Koral Kulacoglu <kulacoglukoral@gmail.com>
2025-12-15 06:08:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 107.1 MB (53.23%)

--- leetcode-solutions/2233-number-of-smooth-descent-periods-of-a-stock/solution.cpp
+++ leetcode-solutions/2233-number-of-smooth-descent-periods-of-a-stock/solution.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     long long getDescentPeriods(vector<int>& prices) {
         int n = prices.size();
-        
         long long ans = 1;
-        long long cnt = 1;
+        int streak = 1;
         for (int i=1; i<n; i++) {
-            if (prices[i] == prices[i-1]-1) {
-                cnt++;
-            }
-            else {
-                cnt = 1;
-            }
-            ans += cnt;
+            if (prices[i] == prices[i-1]-1) streak++;
+            else streak = 1;
+            ans += streak;
         }
-        
         return ans;
     }
 };
