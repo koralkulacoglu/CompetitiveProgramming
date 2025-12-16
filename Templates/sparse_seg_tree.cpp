9796f6b194f7aac80f66ec825ac656f6d7c9a142
Koral Kulacoglu <kulacoglukoral@gmail.com>
2025-12-16 01:25:39 -0500

    modify sparse segtree template to use vector instead of tree nodes

--- Templates/sparse_seg_tree.cpp
+++ Templates/sparse_seg_tree.cpp
@@ -6,64 +6,76 @@ class SparseSegtree {
 	struct Node {
 		int freq = 0;
 		int lazy = 0;
-		Node *left = nullptr;
-		Node *right = nullptr;
+		int left = -1;
+		int right = -1;
 	};
-	Node *root = new Node;
+	vector<Node> tree;
 	const int n;
+	int timer = 0;
 
 	int comb(int a, int b) { return a + b; }
 
-	void apply(Node *cur, int len, int val) {
+	void apply(int cur, int len, int val) {
 		if (val == 1) {
-			(cur->lazy) = val;
-			(cur->freq) = len * val;
+			tree[cur].lazy = val;
+			tree[cur].freq = len * val;
 		}
 	}
 
-	void push_down(Node *cur, int l, int r) {
-		if ((cur->left) == nullptr) { (cur->left) = new Node; }
-		if ((cur->right) == nullptr) { (cur->right) = new Node; }
+	void push_down(int cur, int l, int r) {
+		if (tree[cur].left == -1) {
+			tree[cur].left = ++timer;
+			tree.push_back(Node());
+		}
+		if (tree[cur].right == -1) {
+			tree[cur].right = ++timer;
+			tree.push_back(Node());
+		}
 		int m = (l + r) / 2;
-		apply(cur->left, m - l + 1, cur->lazy);
-		apply(cur->right, r - m, cur->lazy);
+		apply(tree[cur].left, m - l + 1, tree[cur].lazy);
+		apply(tree[cur].right, r - m, tree[cur].lazy);
+		tree[cur].lazy = 0;
 	}
 
-	void range_set(Node *cur, int l, int r, int ql, int qr, int val) {
+	void range_set(int cur, int l, int r, int ql, int qr, int val) {
 		if (qr < l || ql > r) { return; }
 		if (ql <= l && r <= qr) {
 			apply(cur, r - l + 1, val);
 		} else {
 			push_down(cur, l, r);
 			int m = (l + r) / 2;
-			range_set(cur->left, l, m, ql, qr, val);
-			range_set(cur->right, m + 1, r, ql, qr, val);
-			(cur->freq) = comb((cur->left)->freq, (cur->right)->freq);
+			range_set(tree[cur].left, l, m, ql, qr, val);
+			range_set(tree[cur].right, m + 1, r, ql, qr, val);
+			tree[cur].freq =
+			    comb(tree[tree[cur].left].freq, tree[tree[cur].right].freq);
 		}
 	}
 
-	int range_sum(Node *cur, int l, int r, int ql, int qr) {
+	int range_sum(int cur, int l, int r, int ql, int qr) {
 		if (qr < l || ql > r) { return 0; }
-		if (ql <= l && r <= qr) { return cur->freq; }
+		if (ql <= l && r <= qr) { return tree[cur].freq; }
 		push_down(cur, l, r);
 		int m = (l + r) / 2;
-		return comb(range_sum(cur->left, l, m, ql, qr),
-		            range_sum(cur->right, m + 1, r, ql, qr));
+		return comb(range_sum(tree[cur].left, l, m, ql, qr),
+		            range_sum(tree[cur].right, m + 1, r, ql, qr));
 	}
 
   public:
-	SparseSegtree(int n) : n(n) {}
+	SparseSegtree(int n, int q = 0) : n(n) {
+		if (q > 0) { tree.reserve(2 * q * __lg(n)); }
+		tree.push_back(Node());
+	}
 
-	void range_set(int ql, int qr, int val) { range_set(root, 0, n - 1, ql, qr, val); }
+	void range_set(int ql, int qr, int val) { range_set(0, 0, n - 1, ql, qr, val); }
 
-	int range_sum(int ql, int qr) { return range_sum(root, 0, n - 1, ql, qr); }
+	int range_sum(int ql, int qr) { return range_sum(0, 0, n - 1, ql, qr); }
 };
 
 int main() {
 	int query_num;
 	cin >> query_num;
 	const int RANGE_SIZE = 1e9;
-	SparseSegtree st(RANGE_SIZE + 1);
+	SparseSegtree st(RANGE_SIZE + 1, query_num);
 
 	int c = 0;
 	for (int i = 0; i < query_num; i++) {
