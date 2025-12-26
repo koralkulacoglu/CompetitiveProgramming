21bf3c5debf33d6d0227b23bce9bc9e31f9a0b4c
Koral Kulacoglu <kulacoglukoral@gmail.com>
2025-12-26 15:36:21 -0500

    shortest path and MST practice

---
+++ CSES/investigation.cpp
@@ -0,0 +1,259 @@
+/*
+ID: Koral Kulacoglu
+TASK: test
+LANG: C++                 
+*/
+
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef complex<ld> cd;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pld;
+ 
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<char> vc;
+typedef vector<ld> vld;
+typedef vector<ll> vll;
+typedef vector<pi> vpi;
+typedef vector<pll> vpll;
+typedef vector<cd> vcd;
+
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ 
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i, a, b) for (int i=(a)-1; i >= b; i--)
+#define F0Rd(i, a) for (int i=(a)-1; i >= 0; i--)
+#define trav(a, x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+ 
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define mp make_pair
+#define pb push_back
+#define fir first
+#define sec second
+#define ins insert
+#define lbound(a, v) lower_bound(all(a), v)-a.begin()
+#define ubound(a, v) upper_bound(all(a), v)-a.begin()
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a*b)/gcd(a, b)
+ 
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x);
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifdef DEBUG
+#define dbg(x...) cerr <<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << endl;
+#else
+#define dbg(x...)
+#endif
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+struct custom_hash {
+    size_t operator()(uint64_t x) const {
+        x ^= rng();
+        return x ^ (x >> 16);
+    }
+};
+ 
+const int MOD = 1000000007;
+const char nl = '\n';
+const int MX = 100001; 
+
+vll minPrices(vector<vpi>& flights) {
+	int n = sz(flights);
+
+	vll prices(n, 1e18);
+	prices[0] = 0;
+	priority_queue<pll, vpll, greater<pll>> pq;
+	pq.push({0, 0});
+	while (!pq.empty()) {
+		pll node = pq.top(); pq.pop();
+		ll price = node.fir;
+		int city = node.sec;
+		if (price > prices[city]) continue;
+
+		trav (flight, flights[city]) {
+			int newCity = flight.fir;
+			ll newPrice = price + flight.sec;
+			if (newPrice >= prices[newCity]) continue;
+
+			prices[newCity] = newPrice;
+			pq.push({newPrice, newCity});
+		}
+	}
+
+	return prices;
+}
+
+int minFlights(vector<vpi>& flights) {
+    int n = sz(flights);
+    vll prices(n, 1e18);
+    prices[0] = 0;
+    vi minFlights(n, 1e9);
+    minFlights[0] = 0;
+    priority_queue<vll, vector<vll>, greater<vll>> pq; // price, flights, city
+    pq.push({0, 0, 0});
+
+    while (!pq.empty()) {
+        vll node = pq.top(); pq.pop();
+        ll price = node[0];
+        int flightCount = node[1];
+        int city = node[2];
+
+        if (price > prices[city]) continue;
+        if (price == prices[city] && flightCount > minFlights[city]) continue;
+
+        trav (flight, flights[city]) {
+            int newCity = flight.fir;
+            ll newPrice = price + flight.sec;
+            int newFlightCount = flightCount + 1;
+
+            if (newPrice < prices[newCity]) {
+                prices[newCity] = newPrice;
+                minFlights[newCity] = newFlightCount;
+                pq.push({newPrice, newFlightCount, newCity});
+            } else if (newPrice == prices[newCity]) {
+                if (newFlightCount < minFlights[newCity]) {
+                    minFlights[newCity] = newFlightCount;
+                    pq.push({newPrice, newFlightCount, newCity});
+                }
+            }
+        }
+    }
+
+    return minFlights[n-1];
+}
+
+int maxFlights(vector<vpi>& flights) {
+    int n = sz(flights);
+    vll prices(n, 1e18);
+    prices[0] = 0;
+    vi maxFlights(n, -1e9);
+    maxFlights[0] = 0;
+    priority_queue<vll, vector<vll>, greater<vll>> pq; // price, -flights, city
+
+    pq.push({0, 0, 0});
+
+    while (!pq.empty()) {
+        vll node = pq.top(); pq.pop();
+        ll price = node[0];
+        int flightCount = -node[1];
+        int city = node[2];
+
+        if (price > prices[city]) continue;
+        if (price == prices[city] && flightCount < maxFlights[city]) continue;
+
+        trav (flight, flights[city]) {
+            int newCity = flight.fir;
+            ll newPrice = price + flight.sec;
+            int newFlightCount = flightCount + 1;
+
+            if (newPrice < prices[newCity]) {
+                prices[newCity] = newPrice;
+                maxFlights[newCity] = newFlightCount;
+                pq.push({newPrice, -newFlightCount, newCity});
+            } else if (newPrice == prices[newCity]) {
+                if (newFlightCount > maxFlights[newCity]) {
+                    maxFlights[newCity] = newFlightCount;
+                    pq.push({newPrice, -newFlightCount, newCity});
+                }
+            }
+        }
+    }
+
+    return maxFlights[n-1];
+}
+
+vll routes;
+
+ll dfs(int city, vector<vpi>& flights, vll& prices) {
+	int n = sz(flights);
+
+	if (city == n-1) return 1;
+	if (routes[city] != -1) return routes[city];
+
+	ll res = 0;
+	trav (neigh, flights[city]) {
+		int newCity = neigh.fir;
+		ll newPrice = prices[city] + neigh.sec;
+
+		if (newPrice != prices[newCity]) continue;
+
+		res += dfs(newCity, flights, prices);
+		res %= MOD;
+	}
+
+	return routes[city] = res;
+}
+
+void solve() {
+	int n, m; cin >> n >> m;
+
+	vector<vpi> flights(n);
+	FOR (i, 0, m) {
+		int a, b, c; cin >> a >> b >> c; a--; b--;
+		flights[a].pb({b, c});
+	}
+
+	vll prices = minPrices(flights);
+	ll minPrice = prices[n-1];
+
+	routes.resize(n, -1);
+	ll routeCount = dfs(0, flights, prices);
+
+	int minFlight = minFlights(flights);
+
+	int maxFlight = maxFlights(flights);
+
+	cout << minPrice << ' ' << routeCount << ' ' << minFlight << ' ' << maxFlight << nl;
+}
+ 
+int main() {
+    cin.tie(0)->sync_with_stdio(0); 
+    cin.exceptions(cin.failbit);
+ 
+    int T = 1;
+//    cin >> T;
+    while(T--) {
+        solve();
+    }
+ 
+	return 0;
+}
+
