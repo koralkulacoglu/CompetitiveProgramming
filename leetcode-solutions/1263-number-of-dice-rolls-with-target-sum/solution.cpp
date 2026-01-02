f83ec23d940cbf834134f77a76cdc413e8281ded
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-01-02 23:53:29 +0000

    Sync LeetCode submission Runtime - 27 ms (17.69%), Memory - 11.4 MB (35.75%)

--- leetcode-solutions/1263-number-of-dice-rolls-with-target-sum/solution.cpp
+++ leetcode-solutions/1263-number-of-dice-rolls-with-target-sum/solution.cpp
@@ -1,22 +1,18 @@
 class Solution {
+    int MOD = 1e9+7;
 public:
     int numRollsToTarget(int n, int k, int target) {
-        long long MOD = 1e9+7;
-        vector<vector<long long>> dp(n+1, vector<long long>(target+1, 0));
-
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
         dp[0][0] = 1;
-        for (int i=1; i<=min(k, target); i++) dp[1][i] = 1;
-        
-        for (int i=2; i<=n; i++) {
-            for (int j=i; j<=target; j++) {
-                for (int l=1; l<=min(j, k); l++) {
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=target; j++) {
+                for (int l=1; l<=k; l++) {
+                    if (l > j) break;
                     dp[i][j] += dp[i-1][j-l];
                     dp[i][j] %= MOD;
                 }
             }
         }
-
         return dp[n][target];
     }
 };
-
