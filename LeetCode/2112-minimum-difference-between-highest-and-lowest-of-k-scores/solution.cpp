17288e2db406e608eb4e7f87374748cd602f4599
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-01-25 20:06:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.1 MB (90.18%)

---
+++ LeetCode/2112-minimum-difference-between-highest-and-lowest-of-k-scores/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumDifference(vector<int>& nums, int k) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int ans = 1e9;
+        for (int i=k-1; i<n; i++) {
+            ans = min(ans, nums[i] - nums[i-k+1]);
+        }
+        return ans;
+    }
+};
