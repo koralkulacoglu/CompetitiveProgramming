2c131f2490e57c2c7cc3d14555123b92d9ef8239
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-02-10 00:53:22 +0000

    Sync LeetCode submission Runtime - 2101 ms (12.62%), Memory - 473.1 MB (69.84%)

---
+++ LeetCode/4045-longest-balanced-subarray-i/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+        for (int i=0; i<n; i++) {
+            set<int> even, odd;
+            for (int j=i; j<n; j++) {
+                if (nums[j]%2) odd.insert(nums[j]);
+                else even.insert(nums[j]);
+                if (even.size() == odd.size()) ans = max(ans, j - i + 1);
+            }
+        }
+        return ans;
+    }
+};
