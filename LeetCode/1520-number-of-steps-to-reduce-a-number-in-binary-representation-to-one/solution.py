0c39294f3d2584f027719f8bec9a778bbeb47bbe
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-02-26 05:39:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.4 MB (54.01%)

---
+++ LeetCode/1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def numSteps(self, s: str) -> int:
+        ans = 0
+        n = int(s, 2)
+        while n > 1:
+            if n & 1:
+                n += 1
+            else:
+                n >>= 1
+            ans += 1
+        return ans
