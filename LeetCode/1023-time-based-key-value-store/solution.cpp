f67f405d1e469b2542c5fd70fa80b64d8952a3e1
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-04-09 04:30:28 +0000

    Sync LeetCode submission Runtime - 76 ms (15.94%), Memory - 140 MB (5.35%)

--- LeetCode/1023-time-based-key-value-store/solution.cpp
+++ LeetCode/1023-time-based-key-value-store/solution.cpp
@@ -1,16 +1,14 @@
 class TimeMap {
-public:
     unordered_map<string, set<pair<int, string>>> mp;
+public:
     void set(string key, string value, int timestamp) {
         mp[key].insert({timestamp, value});
     }
     
     string get(string key, int timestamp) {
-        auto& st = mp[key];
-        auto ptr = st.upper_bound({timestamp, string(1, char(255))});
-        if (ptr == st.begin()) return "";
-        ptr--;
-        return ptr->second;
+        auto it = mp[key].upper_bound({timestamp, string(1, char(255))});
+        if (it == mp[key].begin()) return "";
+        return (--it)->second;
     }
 };
 
