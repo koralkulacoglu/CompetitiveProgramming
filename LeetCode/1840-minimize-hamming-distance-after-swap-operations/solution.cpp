60970c767c48ea7ec153ab79c6ee3c471b69645c
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-04-21 08:51:20 +0000

    Sync LeetCode submission Runtime - 148 ms (39.83%), Memory - 162.4 MB (13.09%)

---
+++ LeetCode/1840-minimize-hamming-distance-after-swap-operations/solution.cpp
@@ -0,0 +1,51 @@
+struct DSU {
+	vector<int> parents, sizes;
+
+	DSU(int n=2e5) : parents(n), sizes(n, 1) {
+		for (int i=0; i<n; i++) parents[i] = i;
+	}
+
+	int find(int x) {
+		return parents[x] == x ? x : (parents[x] = find(parents[x]));
+	}
+
+	bool unite(int x, int y) {
+		int x_root = find(x);
+		int y_root = find(y);
+		if (x_root == y_root) return false;
+
+		if (sizes[x_root] < sizes[y_root]) swap(x_root, y_root);
+		sizes[x_root] += sizes[y_root];
+		parents[y_root] = x_root;
+
+		return true;
+	}
+
+	bool connected(int x, int y) {
+		return find(x) == find(y);
+	}
+};
+
+class Solution {
+public:
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        int n = source.size();
+        DSU dsu(n);
+        for (vector<int>& swap : allowedSwaps) dsu.unite(swap[0], swap[1]);
+        for (int i=0; i<n; i++) dsu.find(i); 
+        vector<vector<int>> sourceGroups(n);
+        vector<unordered_map<int, int>> targetGroups(n);
+        for (int i=0; i<n; i++) {
+            sourceGroups[dsu.parents[i]].push_back(source[i]);
+            targetGroups[dsu.parents[i]][target[i]]++;
+        }
+        int ans = 0;
+        for (int i=0; i<n; i++) {
+            for (int j : sourceGroups[i]) {
+                if (targetGroups[i][j]) targetGroups[i][j]--;
+                else ans++;
+            }
+        }
+        return ans;
+    }
+};
