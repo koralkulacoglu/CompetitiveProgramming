d369423d6d0085f2b1628286b0ea1139a4cffc7c
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-07-09 22:58:15 +0000

    Sync LeetCode submission Runtime - 351 ms (36.56%), Memory - 229.9 MB (21.03%)

--- LeetCode/2364-longest-path-with-different-adjacent-characters/solution.cpp
+++ LeetCode/2364-longest-path-with-different-adjacent-characters/solution.cpp
@@ -1,31 +1,48 @@
 class Solution {
+    int ans;
+    unordered_map<int, vector<int>> tree;
+    string vals;
+
+    // max val of every char
+    pair<char, int> dfs(int node) {
+        char val = vals[node];
+
+        int best(-1e9), secBest(-1e9);
+        pair<char, int> retVal = {val, 1};
+        for (int neigh : tree[node]) {
+            pair<char, int> curVal = dfs(neigh);
+            char c = curVal.first;
+            int score = curVal.second;
+
+            if (c == val) continue;
+
+            retVal.second = max(retVal.second, score + 1);
+
+            if (score >= best) {
+                secBest = best;
+                best = score;
+            }
+            else secBest = max(secBest, score);
+        }
+
+        ans = max(ans, retVal.second);
+        ans = max(ans, best + secBest + 1);
+
+        return retVal;
+    }
+
 public:
     int longestPath(vector<int>& parent, string s) {
         int n = parent.size();
-        unordered_map<int, vector<int>> tree;
-        for (int i=0; i<n; i++) tree[parent[i]].push_back(i);
 
-        int ans = 1;
+        ans = 0;
+        vals = s;
+        tree.clear();
+        for (int i=1; i<n; i++) tree[parent[i]].push_back(i);
 
-        auto dfs = [&](auto&& self, int node) -> pair<char, int> {
-            vector<pair<char, int>> paths;
-            for (int neigh : tree[node]) paths.push_back(self(self, neigh));
-
-            int maxLen = 1;
-            for (auto path : paths) {
-                char prev = path.first;
-                int len = path.second;
-                if (prev == s[node]) continue;
-
-                ans = max(ans, maxLen + len);
-                maxLen = max(maxLen, len + 1);
-            }
-
-            return {s[node], maxLen};
-        };
-
-        dfs(dfs, 0);
+        dfs(0);
 
         return ans;
     }
 };
+
