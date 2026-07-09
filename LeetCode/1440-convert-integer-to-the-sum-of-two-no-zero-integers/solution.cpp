9f9d9e95478a0c412fad536b1db02575a1c3b30d
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-07-09 20:11:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.2 MB (64.57%)

--- LeetCode/1440-convert-integer-to-the-sum-of-two-no-zero-integers/solution.cpp
+++ LeetCode/1440-convert-integer-to-the-sum-of-two-no-zero-integers/solution.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    bool noZero(int x) {
-        string s = to_string(x);
-        for (char c : s) {
-            if (c == '0') return false;
-        }
-        return true;
-    }
-
     vector<int> getNoZeroIntegers(int n) {
+        auto noZero = [](int x) -> bool {
+            while (x) {
+                if (x % 10 == 0) return false;
+                x /= 10;
+            }
+            return true;
+        };
+
         for (int i=1; i<n; i++) {
-            if (noZero(i) && noZero(n-i)) return {i, n-i};
+            if (noZero(i) && noZero(n - i)) return {i, n-i};
         }
+
         return {-1, -1};
     }
 };
