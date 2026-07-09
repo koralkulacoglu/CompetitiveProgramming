a2d0553b7a1f535135b378cfaa65835cba537d4b
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-07-09 23:17:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (7.83%)

--- LeetCode/1448-maximum-69-number/solution.cpp
+++ LeetCode/1448-maximum-69-number/solution.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        string strNum = to_string(num);
-        int n = strNum.size();
+        string sNum = to_string(num);
+        int n = sNum.size();
         for (int i=0; i<n; i++) {
-            if (strNum[i] == '6') {
-                strNum[i] = '9';
-                break;
-            }
+            if (sNum[i] == '9') continue;
+            sNum[i] = '9';
+            return stoi(sNum);
         }
-        return stoi(strNum);
+        return num;
     }
 };
