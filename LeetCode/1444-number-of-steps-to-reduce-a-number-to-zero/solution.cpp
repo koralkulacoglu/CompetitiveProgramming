3256cd9bb89eaf51e4af24da73d9c3d97ce4bbe9
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-07-12 00:24:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (80.53%)

--- LeetCode/1444-number-of-steps-to-reduce-a-number-to-zero/solution.cpp
+++ LeetCode/1444-number-of-steps-to-reduce-a-number-to-zero/solution.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
-    int numberOfSteps(int num) {
-        int ans = 0;
-        while (num > 0) {
-            if (num%2) num--;
-            else num /= 2;
-            ans++;
-        }
-        return ans;
+    int numberOfSteps(int num, int steps=0) {
+        return num == 0 ? steps : numberOfSteps((num % 2 ? num - 1 : num / 2), steps + 1);
     }
 };
