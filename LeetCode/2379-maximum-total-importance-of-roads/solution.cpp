98cd9f5a7bfc199d9f842cb23a839b1e5e6065c0
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-07-12 00:21:36 +0000

    Sync LeetCode submission Runtime - 141 ms (17.70%), Memory - 167 MB (7.74%)

--- LeetCode/2379-maximum-total-importance-of-roads/solution.cpp
+++ LeetCode/2379-maximum-total-importance-of-roads/solution.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        vector<pair<int, int>> vals(n);
-        for (int i=0; i<n; i++) vals[i].first = i;
+        unordered_map<int, vector<int>> graph;
 
-        int m = roads.size();
-        for (int i=0; i<m; i++) {
-            vals[roads[i][0]].second++;
-            vals[roads[i][1]].second++;
+        for (auto& road : roads) {
+            graph[road[0]].push_back(road[1]);
+            graph[road[1]].push_back(road[0]);
         }
-        
-        sort(vals.begin(), vals.end(),
-             [](pair<int, int> const& a, pair<int, int> const& b) {
-             return a.second > b.second;
-        });
-        
-        // for (auto i : vals) cout << i.first << ' ' << i.second << '\n';
-        // cout << '\n';
-        
-        long long ans=0;
-        long long sv = n;
-        for (auto &i : vals) {
-            ans += sv * i.second;
-            sv--;
+
+        vector<int> importance(n);
+        for (int i=0; i<n; i++) {
+            importance[i] = graph[i].size();
         }
-        
+
+        sort(importance.begin(), importance.end());
+
+        long long ans = 0;
+        for (int i=0; i<n; i++) {
+            long long degree = importance[i];
+            ans += degree * (i + 1);
+        }
+
         return ans;
     }
 };
