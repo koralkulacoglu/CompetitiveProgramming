d697953f8731ed7c199e6c289a96f97beed63eee
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-07-13 05:43:26 +0000

    Sync LeetCode submission Runtime - 40 ms (60.82%), Memory - 61.6 MB (96.87%)

--- LeetCode/1014-k-closest-points-to-origin/solution.cpp
+++ LeetCode/1014-k-closest-points-to-origin/solution.cpp
@@ -1,29 +1,16 @@
-struct Compare {
-    int dist(const pair<int, int>& a) const {
-        return a.first * a.first + a.second * a.second;
-    }
-
-    bool operator()(const pair<int, int>& a, const pair<int, int>& b) const {
-        return dist(a) < dist(b);
-    }
-};
-
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, Compare> pq;
+        auto dist = [](vector<int>& x) -> int {
+            return x[0] * x[0] + x[1] * x[1];
+        };
 
-        for (vector<int>& p : points) {
-            pq.push({p[0], p[1]});
-            if (pq.size() > k) pq.pop();
-        }
+        sort(points.begin(), points.end(), [&](auto& a, auto& b) {
+            return dist(a) < dist(b);
+        });
 
-        vector<vector<int>> res;
-        while (!pq.empty()) {
-            pair<int, int> p = pq.top(); pq.pop();
-            res.push_back({p.first, p.second});
-        }
-
+        vector<vector<int>> res(points.begin(), points.begin() + k);
+        
         return res;
     }
 };
