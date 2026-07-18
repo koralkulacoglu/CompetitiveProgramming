ef0925c0c5f27674406a12ac9f2e4793cb6ed3ed
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-07-18 22:49:25 +0000

    Sync LeetCode submission Runtime - 3 ms (4.69%), Memory - 16.2 MB (34.83%)

--- LeetCode/2106-find-greatest-common-divisor-of-array/solution.cpp
+++ LeetCode/2106-find-greatest-common-divisor-of-array/solution.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int findGCD(vector<int>& nums) {
-        return gcd(*max_element(nums.begin(), nums.end()), *min_element(nums.begin(), nums.end()));
+        int mn = *min_element(nums.begin(), nums.end());
+        int mx = *max_element(nums.begin(), nums.end());
+
+        int ans = 1;
+        for (int i=2; i<=mx; i++) {
+            if (mn % i == 0 && mx % i == 0) ans = i;
+        }
+        
+        return ans;
     }
 };
