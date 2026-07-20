e8b6bb9949607b9e743533910dc056c318ff9be5
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-07-20 23:19:08 +0000

    Sync LeetCode submission Runtime - 7 ms (83.49%), Memory - 23.1 MB (34.91%)

--- LeetCode/2190-count-common-words-with-one-occurrence/solution.cpp
+++ LeetCode/2190-count-common-words-with-one-occurrence/solution.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int countWords(vector<string>& words1, vector<string>& words2) {
-        unordered_map<string, pair<int, int>> freq;
-        for (string& s : words1) freq[s].first++;
-        for (string& s : words2) freq[s].second++;
+        unordered_map<string, int> freq1, freq2;
+        for (string& word : words1) freq1[word]++;
+        for (string& word : words2) freq2[word]++;
+
         int ans = 0;
-        for (auto& p : freq) ans += (p.second.first == 1 && p.second.second == 1);
+        for (auto p : freq1) {
+            int f1 = p.second;
+            int f2 = freq2[p.first];
+            ans += (f1 == 1 && f2 == 1);
+        }
+        
         return ans;
     }
 };
