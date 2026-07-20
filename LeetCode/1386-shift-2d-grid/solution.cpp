3a88933efa84440b7341e2993a48564654513934
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-07-20 23:14:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 18.2 MB (36.52%)

--- LeetCode/1386-shift-2d-grid/solution.cpp
+++ LeetCode/1386-shift-2d-grid/solution.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        int n=grid.size(), m=grid[0].size();
-        vector<vector<int>> nGrid(n, vector<int>(m));
+        int n = grid.size();
+        int m = grid[0].size();
+
+        auto shift = [&](int x, int y) {
+            int val = x * m + y;
+            val += k;
+            val %= (n * m);
+            return make_pair(val / m, val % m);
+        };
+
+        vector<vector<int>> arr(grid.begin(), grid.end());
         for (int i=0; i<n; i++) {
             for (int j=0; j<m; j++) {
-                int cnt = (j+k)/m;
-                nGrid[(i+cnt)%n][(j+k)%m] = grid[i][j];
+                auto [x, y] = shift(i, j);
+                arr[x][y] = grid[i][j];
             }
         }
-        return nGrid;
+
+        return arr;
     }
 };
