957239df0996cf21fc017c716b4181bebedaa631
Koral Kulacoglu <kulacoglukoral@gmail.com>
2026-08-30 06:08:05 +0000

    Sync LeetCode submission Runtime - 16 ms (91.02%), Memory - 48.1 MB (87.79%)

--- LeetCode/0146-lru-cache/solution.cpp
+++ LeetCode/0146-lru-cache/solution.cpp
@@ -1,46 +1,38 @@
+
 class LRUCache {
-    int n;
-    unordered_map<int, list<pair<int, int>>::iterator> cache;
-    list<pair<int, int>> window;
+    int capacity;
+    list<int> lru; 
+    unordered_map<int, pair<int, list<int>::iterator>> cache;
 
 public:
-    LRUCache(int capacity) : n(capacity) {}
-    
+    LRUCache(int capacity) : capacity(capacity) {}
+
     int get(int key) {
         auto it = cache.find(key);
         if (it == cache.end()) return -1;
-        window.splice(window.end(), window, it->second);
-        return it->second->second;
+
+        lru.splice(lru.begin(), lru, it->second.second);
+
+        return it->second.first;
     }
-    
+
     void put(int key, int value) {
         auto it = cache.find(key);
 
         if (it != cache.end()) {
-            window.erase(it->second);
-            window.push_back(make_pair(key, value));
-            cache[key] = prev(window.end());
+            lru.splice(lru.begin(), lru, it->second.second);
+            it->second.first = value;
+            return;
         }
-        else {
-            window.push_back(make_pair(key, value));
-            auto it = window.end();
-            it--;
-            cache[key] = it;
 
-            if (window.size() <= n) return;
-
-            auto it2 = window.front();
-            int key = it2.first;
-            
-            cache.erase(key);
-            window.pop_front();
+        if (cache.size() == capacity) {
+            int keyToDelete = lru.back();
+            lru.pop_back();
+            cache.erase(keyToDelete);
         }
+
+        lru.push_front(key);
+        cache[key] = {value, lru.begin()};
     }
 };
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache* obj = new LRUCache(capacity);
- * int param_1 = obj->get(key);
- * obj->put(key,value);
- */
